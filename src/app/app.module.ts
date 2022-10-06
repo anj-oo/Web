99d095a4a6155c6dadf0e02c02201250282177c1
Anju K V <anjukv@Anjus-MacBook-Pro.local>
2022-10-06 11:20:00 +0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
+const routes: Routes = [
+  {path: '', redirectTo:'Home', pathMatch: "full"},
+  {path: 'Home', component: HomeComponent},
+  {path: 'About', component: AboutComponent},
+  {path: 'Contact', component: ContactComponent},
+  {path: '**', component: ErrorPageComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
